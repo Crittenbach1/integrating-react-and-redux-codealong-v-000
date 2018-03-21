326e673e6db009a9f93ccd620f08ef5411af4df8
Cynthia Rittenbach <crittenbach@gmail.com>
2018-03-21 10:25:35 +0000

    automatically backed up by learn

--- redux-pattern.js
+++ redux-pattern.js
@@ -1,16 +1,4 @@
 
-function changeCount(state = {
-  count: 0,
-}, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-    default:
-      return state;
-  };
-};
-
-
 function render(){
   let container = document.getElementById('container');
   container.innerHTML = store.getState().count;
